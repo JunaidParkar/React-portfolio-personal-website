484aa48b27dcdb4e68f49fa6457159bf5934ffc3
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2022-05-09 17:56:12 +0530

    Update firebaseConfig.jsx

--- src/pages/firebaseConfig.jsx
+++ src/pages/firebaseConfig.jsx
@@ -3,13 +3,7 @@ import { getDatabase } from "firebase/database"
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASFzJ5TuuEVEJT4V8hWz9XpOPsB4dl4xE",
-  authDomain: "verix-d8f82.firebaseapp.com",
-  projectId: "verix-d8f82",
-  storageBucket: "verix-d8f82.appspot.com",
-  messagingSenderId: "1057724321461",
-  appId: "1:1057724321461:web:c7a001b43835e506a7b8fc",
-  measurementId: "G-4WT786C0NG"
+  "your firebase credentials here"
 };
 
 const app = initializeApp(firebaseConfig);
